c6a3090e3d8005cf647351111c977fc9e9800cb0
= <=>
2024-05-03 11:14:22 +0200

    Build information generator

--- compile.bat
+++ compile.bat
@@ -2,5 +2,21 @@
 cls
 echo Compiling main project
 
+REM Get current date and time
+REM Made by chatgpt lol
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
+
+REM Extract date components
+set year=%datetime:~0,4%
+set month=%datetime:~4,2%
+set day=%datetime:~6,2%
+
+REM Extract time components
+set hour=%datetime:~8,2%
+set minute=%datetime:~10,2%
+
+REM Echo formatted date and time
+echo Alpha build %day%%month%%year%-%hour%%minute%
+
 REM compile *.c, link with ws2_32 (socket lib) and treat warnings as errors
 gcc Main.c src\Socket.c src\Funnies.c -o Jailbreak.exe -lws2_32 -lWinmm -Wall -Werror
