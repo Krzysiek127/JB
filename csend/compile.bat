0f4c56c1dd70af715b3161fc9179219f191ce47a
= <=>
2024-05-04 13:07:31 +0200

    CSend removed

--- csend/compile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cls
-echo Compiling csend
-
-REM compile csend.c with Telthar socket wrapper, link with ws2_32 (socket lib) and treat warnings as errors
-gcc csend.c ..\src\Socket.c -o csend.exe -lws2_32 -Wall -Werror
